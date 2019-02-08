edcd820b69ef58ad480d0165399bf2c1b09a3b23
Jenny Chu <jchudesign0930@gmail.com>
2019-02-08 07:56:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,9 @@
 def badge_maker(array)
   array= ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
   array.collect {"Hello my name is" + |x|x }
-  
+  a.map.with_index{ |x, i| x * i }
 end
+
 
 def assign_rooms
   
