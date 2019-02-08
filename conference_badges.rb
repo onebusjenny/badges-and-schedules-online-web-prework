d0a9195c3190e0de835a697b38046ef0520d5b3c
Jenny Chu <jchudesign0930@gmail.com>
2019-02-08 08:39:21 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def badge_maker(array)
+  array= ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
+  array.collect {"Hello my name is" + |x|x }
+  a.map.with_index{ |x, i| x * i }
+end
+
+
+def assign_rooms
+  
+  
+end
+
+
+  
